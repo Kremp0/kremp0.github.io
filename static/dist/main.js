6097d2e1f64d12f4cfc3df03028d22b6bf45e0ad
krempo <samuel.krempasky@gmail.com>
2021-10-17 14:22:11 +0200

    patch testing

--- static/dist/main.js
+++ static/dist/main.js
@@ -1,2 +1,3 @@
 "use strict";
 console.log("hello from the main.ts");
+console.log("Hello this is for the patch");
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,2 +1,4 @@
 console.log("hello from the main.ts");
 
+console.log("Hello this is for the patch");
+
